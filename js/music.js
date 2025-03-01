6ea204b1d97d26f12a56ec0b812e62205c6884a5
qinglin <zql308@qq.com>
2025-03-01 14:59:06 +0800

    Site updated: 2025-03-01 14:58:58

---
+++ js/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: '/musicsource/だんご大家族 - Key Sounds Label.mp3',
+        cover: '/img/avtar.jpg',
+    }]
+});
